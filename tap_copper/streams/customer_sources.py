1ee6b8712b5f3c095608234f5f6fdaf7b26bda49
Alex Caruso <acaruso7@users.noreply.github.com>
2020-07-29 15:57:00 -0400

    Add customer_sources stream

---
+++ tap_copper/streams/customer_sources.py
@@ -0,0 +1,32 @@
+import singer
+
+from tap_copper.streams.base import BaseStream
+
+LOGGER = singer.get_logger()
+
+
+class CustomerSourcesStream(BaseStream):
+    API_METHOD = "GET"
+    TABLE = "customer_sources"
+    KEY_PROPERTIES = ["id"]
+
+    @property
+    def path(self):
+        return "/customer_sources"
+
+    def sync_data(self):
+        table = self.TABLE
+
+        LOGGER.info("Syncing data for {}".format(table))
+        url = self.get_url()
+
+        response = self.client.make_request(url, self.API_METHOD)
+        transformed = self.get_stream_data(response)
+
+        with singer.metrics.record_counter(endpoint=table) as counter:
+            singer.write_records(table, transformed)
+            counter.increment(len(transformed))
+
+        LOGGER.info("Synced {}".format(table))
+
+        return self.state
